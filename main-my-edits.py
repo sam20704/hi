66919863b7da3f76ec4903f3840318cbbf77d5d2
Sameer <sripadamanisriramasameer@gmail.com>
2025-12-04 07:44:02 +0530

    Create main-my-edits.py for new changes for integration

---
+++ main-my-edits.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI, UploadFile, File
+from pa_validator.validators import (legal_clause_validator, 
+service_description_validator, template_validator)
+import PyPDF2
+from legal_redline_diff_engine import get_legal_redline_for_document
+app = FastAPI(title="Project Agreement Validator")
+ 
+@app.post("/validate/")
+async def validate_document(file: UploadFile = File(...)):
+ 
+ 
+    # OCR to extract the document contents - ocr folder
+    pdf_reader = PyPDF2.PdfReader(file.file)
+    text = ""
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+    # Utility functions - utils folder
+    ''' 
+    1. Get template document - return template
+    2. Tag different sections - return sections with tags
+ 
+    '''
+ 
+ 
+    # Validaton logic - validators folder
+    legal_status, legal_changes = legal_clause_validator.validate(text)
+    service_status, service_changes = service_description_validator.validate(text)
+    # Intermediate JSON structure for template validation
+    template_details = template_validator(text)
+    legal_redline_result = get_legal_redline_for_document(text)
+
+ 
+    # Final JSON response structure
+    response = {
+        "template_details": template_details,
+        "legal_clauses": {
+            "status": legal_status,
+            "changes": legal_changes
+        },
+        "service_description": {
+            "status": service_status,
+            "changes": service_changes
+        },
+        "legal_redline_diff": legal_redline_result
+    }
+    return response
